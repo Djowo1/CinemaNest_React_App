f33194602a8fcfcd7fd52834e72179c17ff68db8
Djowo1 <itzdjowo@gmail.com>
2025-07-19 16:31:30 +0100

    updated for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
+  base: '/CinemaNest_React_App/'
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
 });
