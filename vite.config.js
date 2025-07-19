7b39e1b5698601384de2441491fa75762e573771
Djowo1 <itzdjowo@gmail.com>
2025-07-19 16:55:19 +0100

    updated again for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: '/CinemaNest_React_App/'
+  base: '/CinemaNest_React_App/',
   plugins: [react()],
 });
