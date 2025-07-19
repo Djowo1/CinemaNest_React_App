d52bbe9ff1292b72d7dba176a1ce47a96b81ee0f
Djowo1 <itzdjowo@gmail.com>
2025-07-19 17:44:06 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
@@ -27,13 +27,13 @@ if(user){
   return (
     <div>
       <ToastContainer theme='dark' />
-     <Router>
+    
        <Routes>
         <Route path='/' element={ <Home />}/>
         <Route path='/login' element={ <Login />}/>
         <Route path='/player/:id' element={ <Player />}/>
       </Routes>
-      </Router>
+     
     
     </div>
   )
